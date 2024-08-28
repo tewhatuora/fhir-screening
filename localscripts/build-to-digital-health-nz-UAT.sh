27b17d85b847cca275d1b4b1aabcca0dcbec2f24
David Grainger <david.grainger@middleware.co.nz>
2024-08-28 18:01:34 +1000

    remove local scripts

--- localscripts/build-to-digital-health-nz-UAT.sh
+++
@@ -1,4 +0,0 @@
-curl --location 'https://fhir-ig-builder.api-uat.digital.health.nz/v1/build' \
---header 'x-api-key: rCxZD9X41qWHRcZw1l1Aa9PmIZt8PRPaOAWixTF6' \
---header 'Content-Type: application/json' \
---data '{"igSourceGitUrl":"https://github.com/tewhatuora/fhir-screening.git", "gitBranch":"specPractitionerAcceptance", "publishToRegistry": false}'
--- localscripts/publishRFBranch.sh
+++
@@ -1,7 +0,0 @@
-### Setting up a webhook for automatic branch publishing
-# Change "ref" value to refs/heads/<remote branch name>
-
-#!/bin/bash
-curl -X POST  "https://us-central1-fhir-org-starter-project.cloudfunctions.net/ig-commit-trigger" \
-  -H "Content-type: application/json" \
-  --data '{"ref": "refs/heads/main", "repository": {"full_name": "tewhatuora/rheumatic-fever"}}'
